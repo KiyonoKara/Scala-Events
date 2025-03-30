31a3e1845ff2be1ab21372dd0088c82c30291bc8
Kiyo <68307546+KiyonoKara@users.noreply.github.com>
2025-03-30 01:22:10 -0400

    Feature: LiteEmitter class (to see how much code I can not use and still use the features)

---
+++ src/main/scala/org/kiyo/events/LiteEmitter.scala
@@ -0,0 +1,47 @@
+package org.kiyo.events
+
+import java.util.UUID
+import scala.collection.mutable
+
+/**
+ * The lighter version of the original Emitter, has the same features but 
+ * @tparam T The type of data to emit
+ */
+class LiteEmitter[T] {
+  private val callbacks: mutable.Map[String, mutable.Buffer[(String, Callback[T])]] = mutable.Map.empty
+  final val ALL: String = "ALL_CHANNELS" + UUID.randomUUID().toString
+
+  //noinspection DuplicatedCode
+  def emit(channel: String, data: T): Unit = {
+    val channelsToEmit: Seq[String] = if (channel.equals(this.ALL)) this.callbacks.keys.toSeq else Seq(this.ALL, channel)
+    channelsToEmit.flatMap(this.callbacks.get).foreach(callbackPairs => {
+      callbackPairs.foreach((_, callback) => {
+        callback.call(channel, data)
+      })
+    })
+  }
+
+  def on(channel: String, callback: Callback[T]): String = {
+    val uuid: String = UUID.randomUUID().toString
+    val callbackPairBuffer: mutable.Buffer[(String, Callback[T])] = this.callbacks.getOrElseUpdate(channel, mutable.Buffer())
+    callbackPairBuffer += Tuple2(uuid, callback)
+    uuid
+  }
+
+  def off(uuid: String): Unit = {
+    var removedKey: Option[String] = None
+
+    callbacks.foreach({ 
+      case (key, buffer) => 
+      if (buffer.exists(_._1.equals(uuid))) {
+        removedKey = Some(key)
+      }
+    })
+
+    removedKey.foreach(key => {
+      val filteredBuffer = callbacks(key).filter(!_._1.equals(uuid))
+      callbacks.remove(key)
+      callbacks.put(key, filteredBuffer)
+    })
+  }
+}
