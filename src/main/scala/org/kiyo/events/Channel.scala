ceeedbcfe6da45c16b732538c8fb85fe79a0cefd
Kiyo <68307546+KiyonoKara@users.noreply.github.com>
2025-11-03 16:08:10 -0500

    doc: document Channel enum

--- src/main/scala/org/kiyo/events/Channel.scala
+++ src/main/scala/org/kiyo/events/Channel.scala
@@ -1,6 +1,16 @@
 package org.kiyo.events
 
+/**
+ * Type of channel to emit.
+ */
 enum Channel {
+  /**
+   * All channels
+   */
   case ALL
+
+  /**
+   * One or more channels
+   */
   case SOME(channels: String*)
 }
